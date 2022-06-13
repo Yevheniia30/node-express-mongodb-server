16004979a82fd2c4a064e49419e5b2d5a582084c
Yevheniia Suska <susskaya86@gmail.com>
2022-06-14 01:57:25 +0300

    create user and get all users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import mongoose from "mongoose";
+import User from "./User.js";
 
 const PORT = 5000;
 const DB_URL =
@@ -11,13 +12,26 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json("working aaaaaa");
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error.message);
+  }
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.status(200).json(req.body);
+app.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { email, password } = req.body;
+    const user = await User.create({ email, password });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error.message);
+  }
 });
 
 async function startApp() {
